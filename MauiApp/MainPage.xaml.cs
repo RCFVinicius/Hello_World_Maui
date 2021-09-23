cb5d6451a3a09c58e4fdcdbb9cc75ac16b5a8890
Vinicius Vieira <viniciusvvv2@gmail.com>
2021-09-23 11:04:57 -0300

    Primeiro commit

---
+++ MauiApp/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using System;
+
+namespace MauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            CounterLabel.Text = "Hello World";
+
+            SemanticScreenReader.Announce(CounterLabel.Text);
+        }
+    }
+}
